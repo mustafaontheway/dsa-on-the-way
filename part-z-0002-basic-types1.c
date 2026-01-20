17e3366639ee597477a4fde3d151cfabb8233f7a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-01-20 21:16:44 +0300

    Create part-z-0002-basic-types1.c

---
+++ part-z-0002-basic-types1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdint.h>
+
+int main() {
+
+    int year = 2025;
+
+    uint8_t age;
+
+    age = 10;
+
+    uint16_t new_year = 2026;
+
+    printf("%d\n", year);
+    printf("%d\n", age);
+    printf("%d", new_year);
+    
+    return 0;
+}
+
+// 2025
+// 10
+// 2026
